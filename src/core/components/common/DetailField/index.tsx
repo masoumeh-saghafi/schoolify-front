cfa72a2d8b0bd20c4770987006b0a145dc5537c8
Matin Khaleghi <thepokerfa3e@gmail.com>
2025-11-03 00:50:08 +0330

    Fix typo

--- src/core/components/common/DetailField/index.tsx
+++ src/core/components/common/DetailField/index.tsx
@@ -1,32 +1,36 @@
 // src/features/user/profile/components/DetailField.tsx
 
-import Grid from '@schoolify/core/components/base/inputs/Grid'
-import BaseField from '@schoolify/core/components/common/BaseField/index'
+import Grid from "@schoolify/core/components/base/inputs/Grid";
+import BaseField from "@schoolify/core/components/common/BaseField/index";
 
-interface DetailFielddProps {
-   label: string
-   value?: string | number | null
-   xs?: number
-   sm?: number
-   type?: string
-   readOnly?: boolean
+interface DetailFieldProps {
+  label: string;
+  value?: string | number | null;
+  xs?: number;
+  sm?: number;
+  type?: string;
+  readOnly?: boolean;
 }
 
-const DetailField = (props: DetailFielddProps) => {
-   const {
-  label, value, xs = 12, sm = 6, type = 'text', readOnly = true
-}
-=props
+const DetailField = (props: DetailFieldProps) => {
+  const {
+    label,
+    value,
+    xs = 12,
+    sm = 6,
+    type = "text",
+    readOnly = true,
+  } = props;
   return (
-    <Grid size={{ xs, sm }} sx={{ mb:1}}>
+    <Grid size={{ xs, sm }} sx={{ mb: 1 }}>
       <BaseField
         label={label}
-        value={value ? String(value) : ''}
+        value={value ? String(value) : ""}
         type={type}
         readOnly={readOnly}
       />
     </Grid>
-  )
-}
+  );
+};
 
-export default DetailField
+export default DetailField;
