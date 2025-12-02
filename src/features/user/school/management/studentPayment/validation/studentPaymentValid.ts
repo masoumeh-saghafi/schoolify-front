7dc1082e0d6df40361e45659f12b978c3a1dbaa9
Matin Khaleghi <thepokerfa3e@gmail.com>
2025-12-02 20:05:47 +0330

    fix add student payment

--- src/features/user/school/management/studentPayment/validation/studentPaymentValid.ts
+++ src/features/user/school/management/studentPayment/validation/studentPaymentValid.ts
@@ -1,16 +1,24 @@
-import z from 'zod'
+import z from "zod";
 
 const DateValidation = new RegExp(
   `^(13|14)\\d{2}\\/(0[1-9]|1[0-2])\\/(0[1-9]|[12][0-9]|3[01])$`
-)
+);
 
 export const validationSchema = z.object({
   description: z.string().min(2).max(64),
-  amount: z.number(),
-  paymentNumber: z.number(),
-  studentId: z.string().min(1, 'شناسه دانش آموز الزامی است'),
+  amount: z.preprocess(
+    (val) => {
+      const num = Number(val);
+      return isNaN(num) ? undefined : num;
+    },
+    z.number().refine((val) => val !== undefined, {
+      message: "مقدار باید عددی باشد",
+    })
+  ),
+  paymentNumber: z.string(),
+  studentId: z.string().min(1, "شناسه دانش آموز الزامی است"),
   paymentDate: z.string().regex(DateValidation, {
-    message: 'لطفا تاریخ را صحیح وارد کنید'
+    message: "لطفا تاریخ را صحیح وارد کنید",
   }),
-  educationYearId: z.string().min(1, 'سال تحصیلی الزامی است')
-})
+  educationYearId: z.string().min(1, "سال تحصیلی الزامی است"),
+});
