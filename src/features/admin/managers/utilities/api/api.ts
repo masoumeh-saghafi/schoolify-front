b0eaeecd6734529247b31b14642f7c98967ffd7b
Matin Khaleghi <thepokerfa3e@gmail.com>
2025-12-16 21:16:43 +0330

    bugfix remove user role

--- src/features/admin/managers/utilities/api/api.ts
+++ src/features/admin/managers/utilities/api/api.ts
@@ -1,37 +1,30 @@
-import type { BaseRequestPaginationParams } from '@schoolify/core/types/core/api/request'
+import type { BaseRequestPaginationParams } from "@schoolify/core/types/core/api/request";
 import type {
   BaseAddResponseEntity,
-  BaseIdDataEntity
-} from '@schoolify/core/types/core/api/response'
+  BaseIdDataEntity,
+} from "@schoolify/core/types/core/api/response";
 
 import {
   deleteData,
   getListPaginatedData,
-  postData
-} from '@schoolify/core/utilities/api/api'
+  postData,
+} from "@schoolify/core/utilities/api/api";
 
-import adminRoleEndpoints from './endpoints'
-import type ListAdminRolesEntity from '../../types/api/ListAdminRolesEntity'
+import adminRoleEndpoints from "./endpoints";
+import type ListAdminRolesEntity from "../../types/api/ListAdminRolesEntity";
 
 export const addAdminRole = async (data: any) => {
   return await postData<BaseAddResponseEntity>(
     adminRoleEndpoints.addAdminRole,
     data
-  )
-}
+  );
+};
 
-
-export const deleteAdminRole = async (
-  schoolId: string,
-
-) => {
-  return await deleteData<void>(
-    adminRoleEndpoints.deleteAdminRole(schoolId)
-  )
-}
+export const deleteAdminRole = async (userId: string) => {
+  return await postData<void>(adminRoleEndpoints.deleteAdminRole(userId), {});
+};
 
 export const listAdminRoles = async (
-
   pagination: BaseRequestPaginationParams,
   filters: Record<string, string>
 ) => {
@@ -39,5 +32,5 @@ export const listAdminRoles = async (
     adminRoleEndpoints.listAdminRoles,
     pagination,
     filters
-  )
-}
+  );
+};
