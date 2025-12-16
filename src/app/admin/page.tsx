793f5c3f8a7fa21fedacf3b2b48eb408535f662a
Matin Khaleghi <thepokerfa3e@gmail.com>
2025-12-16 20:43:25 +0330

    bugfix redirect to tickets ur;

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,18 @@
 // MUI Components
 
-import AdminTicketPage from "./tickets/page";
+import routes from "@schoolify/core/utilities/routes";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+// import AdminTicketPage from "./tickets/page";
 
 const AdminDashboardPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate(routes.admin.tickets.index());
+  }, []);
   // Render
-  return (
-    <>
-      <AdminTicketPage />
-    </>
-  );
+  return <>{/* <AdminTicketPage /> */}</>;
 };
 
 export default AdminDashboardPage;
